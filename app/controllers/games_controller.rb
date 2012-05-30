c29dfb75a79cc104c301bb8b68cbdbb905726e95
Alex Chvatal <achvatal@redhat.com>
2012-05-30 09:27:02 -0400

    DRY up finding a game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,46 +1,32 @@
 class GamesController < ApplicationController
   respond_to :html, :xml
-
   load_and_authorize_resource
+  before_filter :find_game, :only => [:new, :show, :edit, :update, :destroy, :roll_dice, :cast_shadow, :shed_light, :invite, :uninvite]
 
   # GET /games
   # GET /games.json
   def index
     @games = Game.all
 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @games }
-    end
+    respond_with @games
   end
 
   # GET /games/1
   # GET /games/1.json
   def show
-    @game = Game.find(params[:id])
-    
     @dice_pool = DiceRoller::DicePool.new
 
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @game }
-    end
+    respond_with @game, @dice_pool
   end
 
   # GET /games/new
   # GET /games/new.json
   def new
-    @game = Game.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @game }
-    end
+    respond_with @game
   end
 
   # GET /games/1/edit
   def edit
-    @game = Game.find(params[:id])
   end
 
   # POST /games
@@ -49,49 +35,32 @@ class GamesController < ApplicationController
     @game = Game.new(params[:game])
     @game.gm = current_user
 
-    respond_to do |format|
-      if @game.save
-        format.html { redirect_to @game, notice: 'Game was successfully created.' }
-        format.json { render json: @game, status: :created, location: @game }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @game.errors, status: :unprocessable_entity }
-      end
-    end
+    flash[:notice] = 'Game was successfully created.' if @game.save
+
+    respond_with @game
   end
 
   # PUT /games/1
   # PUT /games/1.json
   def update
-    @game = Game.find(params[:id])
+    flash[:notice] = 'Game was successfully updated.' if @game.save
 
-    respond_to do |format|
-      if @game.update_attributes(params[:game])
-        format.html { redirect_to @game, notice: 'Game was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @game.errors, status: :unprocessable_entity }
-      end
-    end
+    respond_with @game
   end
 
   # DELETE /games/1
   # DELETE /games/1.json
   def destroy
-    @game = Game.find(params[:id])
     @game.destroy
 
     respond_to do |format|
       format.html { redirect_to games_url }
-      format.json { head :no_content }
+      #format.json { head :no_content }
     end
   end
   
   # POST /games/1/roll_dice
   def roll_dice
-    @game = Game.find(params[:id])
-    
     @degree, @wins, @dominating, @discipline, @exhaustion, @madness, @pain = @game.roll(params[:discipline].to_i, params[:exhaustion].to_i, params[:madness].to_i, params[:pain].to_i,)
     @game.save
     
@@ -107,8 +76,6 @@ class GamesController < ApplicationController
   
   # PUT /games/1/cast_shadow
   def cast_shadow
-    @game = Game.find(params[:id])
-    
     @game.cast_shadow(params[:despair_coins].to_i)
     @game.save
     
@@ -119,8 +86,6 @@ class GamesController < ApplicationController
   
   # PUT /games/1/cast_shadow
   def shed_light
-    @game = Game.find(params[:id])
-    
     @game.shed_light(params[:hope_coins].to_i)
     @game.save
     
@@ -131,7 +96,6 @@ class GamesController < ApplicationController
   
   # PUT /games/1/invite
   def invite
-    @game = Game.find(params[:id])
     @email = params[:email]
     
     @game.players << User.find_by_email(@email)
@@ -144,7 +108,6 @@ class GamesController < ApplicationController
   
   # PUT /games/1/uninvite
   def uninvite
-    @game = Game.find(params[:id])
     @email = params[:email]
     user = User.find_by_email(@email)
     @id = user.id
@@ -153,6 +116,17 @@ class GamesController < ApplicationController
     
     respond_to do |format|
       format.js
+    end
+  end
+
+  private
+
+  # sets the game variable as a before_filter
+  def find_game
+    if params[:action] == "new"
+      @game = Game.new
+    else
+      @game = Game.find_by_id(params[:id])
     end
   end
 end
