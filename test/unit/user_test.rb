84defa4eeb18e4323838a2bd104ef20b53acdcdf
Alex Chvatal <m.chvatal@gmail.com>
2012-05-30 22:31:55 -0400

    add basic unit test for users
    we'll let devise handle the rest

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:one)
+  end
+  
+  # assure email addresses are unique
+  test "email uniqueness" do
+    user2 = users(:two)
+    
+    @user.email = user2.email
+    
+    assert (not @user.save), "saved with a duplicate email"
+  end
 end
