aa4159f747dbb2ae76dc984007ed1b19711aabea
Alex Chvatal <m.chvatal@gmail.com>
2012-05-31 21:56:41 -0400

    rescue cancan permission errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,8 @@ class ApplicationController < ActionController::Base
   check_authorization :unless => :devise_controller?
 
   protect_from_forgery
+  
+  rescue_from CanCan::AccessDenied do |exception|
+      redirect_to root_url, :alert => exception.message
+    end
 end
