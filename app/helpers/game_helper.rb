c68f7d91cecd4f39449f9687d875c5cdb3291313
Alex Chvatal <m.chvatal@gmail.com>
2012-06-11 21:09:52 -0400

    Add helper method for recall links

--- app/helpers/game_helper.rb
+++ app/helpers/game_helper.rb
@@ -1,4 +1,10 @@
 # Game helper
 
 module GameHelper
+  # returns an ajax link to recall a scar for a given dice pool
+  def recall_link(game, result, pool)
+    pools = {'discipline' => result.discipline, 'exhaustion' => result.exhaustion, 'madness' => result.madness}
+    
+    escape_javascript(link_to "Recall a scar", recall_game_result_path(game, result, :pool => pool), :method => :put, :remote => true) unless pools[pool].empty?
+  end
 end
