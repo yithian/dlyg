61f2d0749cdb662c7e64a6dd148ebe5a3be3b0b1
Alex Chvatal <achvatal@redhat.com>
2012-06-14 16:51:46 -0400

    connect dlyg to external gmail account

---
+++ config/initializers/mail.rb
@@ -0,0 +1,9 @@
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.gmail.com',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => 'dontloseyourgame@gmail.com',
+  :password       => ENV['OPENSHIFT_DB_PASSWORD'],
+  :domain         => 'gmail.com'
+}
+ActionMailer::Base.delivery_method = :smtp
